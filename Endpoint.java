17268e5edbef01d759b37d59ef7c01a18f144b97
RADRI <radridri@gmail.com>
2022-11-14 16:24:05 +0000

    Make all endpoints one class

--- Endpoint.java
+++ Endpoint.java
@@ -1,25 +1,31 @@
 import java.net.DatagramSocket;
+import java.net.InetAddress;
 import java.net.DatagramPacket;
 import java.net.InetSocketAddress;
 import java.util.HashMap;
 
-public class EndpointB extends Node
+public class Endpoint extends Node
 {
     static final int DEFAULT_SRC_PORT = 50001;
 	static final int DEFAULT_DST_PORT = 54321;
     static final String DEFAULT_DST_NODE = "forwarderone";
-    InetSocketAddress dstAddress;
 
-	Terminal workerTerminal;
+    InetSocketAddress dstAddress;
+	Terminal endpointTerminal;
+    String containerAlias;
+    String aliasLength;
 
     /**
 	 * Constructor
 	 *
 	 * Attempts to create socket at given port and create an InetSocketAddress for the destinations
 	 */
-	EndpointB(Terminal t, String dstHost, int dstPort, int srcPort) {
+	Endpoint(Terminal t, String dstHost, int dstPort, int srcPort) {
 		try {
-			workerTerminal = t;
+            containerAlias = InetAddress.getLocalHost().getHostName();
+            aliasLength = Integer.toString(containerAlias.length());
+
+			endpointTerminal = t;
 			dstAddress= new InetSocketAddress(dstHost, dstPort);
 			socket= new DatagramSocket(srcPort);
 			listener.go();
@@ -32,7 +38,7 @@ public class EndpointB extends Node
 	 */
 	public synchronized void onReceipt(DatagramPacket packet) {
 		try {
-			workerTerminal.println("Received packet");
+			endpointTerminal.println("Received packet");
 
 			PacketContent content= PacketContent.fromDatagramPacket(packet);
 
@@ -42,13 +48,13 @@ public class EndpointB extends Node
 
 				if(type.equals(ACK_PACKET))
 				{
-					workerTerminal.println("From Forwarder: " + tlvs.get(T_MESSAGE));
+					endpointTerminal.println("From Forwarder: " + tlvs.get(T_MESSAGE));
 					this.notify();
 				}
 				else if(type.equals(MESSAGE_PACKET)) //receiving message from somewhere
 				{
 
-					workerTerminal.println("From " + tlvs.get(T_SENDER_NAME) + ": " + tlvs.get(T_MESSAGE));
+					endpointTerminal.println("From " + tlvs.get(T_SENDER_NAME) + ": " + tlvs.get(T_MESSAGE));
 					this.notify();
 				}
 			}
@@ -57,17 +63,17 @@ public class EndpointB extends Node
 	}
 
     public synchronized void start() throws Exception {
-		workerTerminal.println("Welcome!");
+		endpointTerminal.println("Welcome!");
 		while (true) 
 		{
-			workerTerminal.println("Type 'receive' to receive packets, 'send' to send, 'quit' to quit.");
+			endpointTerminal.println("Type 'receive' to receive packets, 'send' to send, 'quit' to quit.");
 
-			String userInput = workerTerminal.read("Type Here");
+			String userInput = endpointTerminal.read("Type Here");
 			if(userInput != null && userInput.equals("quit"))
 			{
-				workerTerminal.println("Disconnecting from network");
+				endpointTerminal.println("Disconnecting from network");
 				DatagramPacket disconnect;
-				String val = T_MESSAGE + "3DIS" + T_CONTAINER + "4ENDB";
+				String val = T_MESSAGE + "3DIS" + T_CONTAINER + aliasLength + containerAlias;
 				disconnect= new TLVPacket(CON_ENDPOINT, "2", val).toDatagramPacket();
 				disconnect.setSocketAddress(dstAddress);
 				socket.send(disconnect);
@@ -76,9 +82,9 @@ public class EndpointB extends Node
 			}
 			else if(userInput != null && userInput.equals("receive"))
 			{
-				workerTerminal.println("Connecting to this network's Forwarder");
+				endpointTerminal.println("Connecting to this network's Forwarder");
 				DatagramPacket connectReceive;
-				String val = T_PORT + "550001" + T_CONTAINER + "4ENDB";
+				String val = T_PORT + "550001" + T_CONTAINER + aliasLength + containerAlias;
 				connectReceive= new TLVPacket(CON_ENDPOINT, "2", val).toDatagramPacket();
 				connectReceive.setSocketAddress(dstAddress);
 				socket.send(connectReceive);
@@ -88,32 +94,32 @@ public class EndpointB extends Node
 			}
 			else if(userInput != null && userInput.equals("send"))
 			{
-				workerTerminal.println("Connecting to this network's Forwarder");
+				endpointTerminal.println("Connecting to this network's Forwarder");
 				DatagramPacket connectSend;
-				String val = T_MESSAGE + "3SEN" + T_CONTAINER + "4ENDB";
+				String val = T_MESSAGE + "3SEN" + T_CONTAINER + aliasLength + containerAlias;
 				connectSend= new TLVPacket(CON_ENDPOINT, "2", val).toDatagramPacket();
 				connectSend.setSocketAddress(dstAddress);
 				socket.send(connectSend);
 
 				this.wait();
 
-				workerTerminal.println("Enter the name of the container the endpoint you want to send to is on.");
+				endpointTerminal.println("Enter the name of the container the endpoint you want to send to is on.");
 				String containerName;
 				while(true)
 				{
-					containerName = workerTerminal.read("Type Name Here");
+					containerName = endpointTerminal.read("Type Name Here");
 					if(containerName == null || containerName.length() <= 3)
-						workerTerminal.println("Invalid Response");
+						endpointTerminal.println("Invalid Response");
 					else
 						break;
 				}
-				workerTerminal.println("Enter the message to send. Message cannot begin with a number.");
+				endpointTerminal.println("Enter the message to send. Message cannot begin with a number.");
 				String message;
 				while(true)
 				{
-					message = workerTerminal.read("Type Message Here");
+					message = endpointTerminal.read("Type Message Here");
 					if(message == null || message.length() < 1)
-						workerTerminal.println("Invalid Response");
+						endpointTerminal.println("Invalid Response");
 					else
 						break;
 				}
@@ -121,7 +127,7 @@ public class EndpointB extends Node
 				String messageLength = Integer.toString(message.length()); 
 
 				DatagramPacket messageSend;
-				val = T_MESSAGE + messageLength + message + T_DEST_NAME + containerLength + containerName + T_SENDER_NAME + "4ENDB";
+				val = T_MESSAGE + messageLength + message + T_DEST_NAME + containerLength + containerName + T_SENDER_NAME + aliasLength + containerAlias;
 				messageSend = new TLVPacket(MESSAGE_PACKET,"3",val).toDatagramPacket();
 				messageSend.setSocketAddress(dstAddress);
 				socket.send(messageSend);
@@ -130,15 +136,15 @@ public class EndpointB extends Node
 			}
 			else
 			{
-				workerTerminal.println("Not a valid input.");
+				endpointTerminal.println("Not a valid input.");
 			}
 		}
 	}
 
     public static void main(String[] args) {
 		try {
-			Terminal endpointTerminal = new Terminal("EndpointB");
-			(new EndpointB(endpointTerminal, DEFAULT_DST_NODE, DEFAULT_DST_PORT, DEFAULT_SRC_PORT)).start();
+			Terminal endpointTerminal = new Terminal("Endpoint");
+			(new Endpoint(endpointTerminal, DEFAULT_DST_NODE, DEFAULT_DST_PORT, DEFAULT_SRC_PORT)).start();
 			endpointTerminal.println("Program completed");
 		} catch(java.lang.Exception e) {e.printStackTrace();}
 	}
